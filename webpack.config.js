c305e71f9830b012db174d09d46f94d500b8e6be
Secret <toontown@inbox.ru>
2020-02-14 20:36:07 +0300

    вариант 3

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const CopyWebpackPlugin= require('copy-webpack-plugin');
 // let isProduction = (process.env.NODE_ENV == "production");
 
 module.exports = {
+  devtool: 'source-map',
   mode: "development",
   entry: {
     // context: path.resolve(__dirname, 'src'),
